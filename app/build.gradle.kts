bf2056248180850ce09b8f4b8d824535e06b8041
gleb <gleb1336@gmail.com>
2024-12-09 19:19:30 +0300

    Add splash animation and app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/ru/gzaytsev/bicycletracker/MainActivity.kt
+++ app/src/main/java/ru/gzaytsev/bicycletracker/MainActivity.kt
@@ -11,11 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.gzaytsev.bicycletracker.ui.theme.BicycleTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition {
+            false
+        }
+        setContent{
+            BicycleTrackerTheme {
+
+            }
+        }
         enableEdgeToEdge()
         setContent {
             BicycleTrackerTheme {
